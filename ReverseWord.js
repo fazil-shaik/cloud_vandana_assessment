f260c6e3c5229798c82e9984b6f215c07e7a05eb
shaik fazil <93812181+fazil-shaik@users.noreply.github.com>
2023-10-25 16:56:45 +0530

    Update ReverseWord.js
    updated code

--- ReverseWord.js
+++ ReverseWord.js
@@ -1,17 +1,40 @@
+function reverseWord(word) {
+  var reversed = '';
+  for (var i = word.length - 1; i >= 0; i--) {
+    reversed += word[i];
+  }
+  return reversed;
+}
 
 function reverseWordsInSentence(sentence) {
-    var words = sentence.split(" ");
-  
-    var reversedWords = words.map(function(word) {
-      return word.split('').reverse().join('');
-    });
-  
-    var reversedSentence = reversedWords.join(" ");
-  
-    return reversedSentence;
+  var words = [];
+  var currentWord = '';
+  for (var i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== ' ') {
+      currentWord += sentence[i];
+    } else {
+      words.push(currentWord);
+      currentWord = '';
+    }
   }
-  
-  var inputSentence = "This is a sunny day";
-  var reversedSentence = reverseWordsInSentence(inputSentence);
-  console.log(reversedSentence); 
-  
+  words.push(currentWord); // Add the last word
+
+  var reversedWords = [];
+  for (var j = 0; j < words.length; j++) {
+    reversedWords.push(reverseWord(words[j]));
+  }
+
+  var reversedSentence = '';
+  for (var k = 0; k < reversedWords.length; k++) {
+    reversedSentence += reversedWords[k];
+    if (k < reversedWords.length - 1) {
+      reversedSentence += ' ';
+    }
+  }
+
+  return reversedSentence;
+}
+
+var inputSentence = "This is a sunny day";
+var reversedSentence = reverseWordsInSentence(inputSentence);
+console.log(reversedSentence);
