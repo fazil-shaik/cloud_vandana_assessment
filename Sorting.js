e44fd31871604e92b0247f0125a059494b65aef3
shaik fazil <93812181+fazil-shaik@users.noreply.github.com>
2023-10-25 16:58:00 +0530

    Update Sorting.js
    updated the changes

--- Sorting.js
+++ Sorting.js
@@ -1,7 +1,17 @@
-let array = [2,3,4,12,4,5,6]
+function bubbleSortDescending(arr) {
+  var len = arr.length;
+  for (var i = 0; i < len - 1; i++) {
+    for (var j = 0; j < len - 1 - i; j++) {
+      if (arr[j] < arr[j + 1]) {
+        // Swap elements if they are in the wrong order (descending order)
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+}
 
-array.sort(function(a,b){
-    return b-a;
-});
-
-console.log("Dedcending order is: "+array);
+var array = [2, 3, 4, 12, 4, 5, 6];
+bubbleSortDescending(array);
+console.log("Descending order is: " + array);
